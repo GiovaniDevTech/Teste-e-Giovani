236fdf30a889cca8a89009bb14884b622b5348de
GiovaniDevTech <giovani.vieira.lavall@outlook.com.br>
2023-02-06 19:42:08 -0300

    Aula3.js
    Operadores Relacionais

---
+++ Giovani JS/Aula3.js
@@ -0,0 +1,18 @@
+//3 ATIVIDADE
+
+Correção:
+//Encontre de 3 a 5 comparações que o resultado é Verdadeiro ou True
+//Encontre de 3 a 5 comparações que o resultado é Falso ou False
+
+//Operadores Relacionais 
+// >, <, <=, >=, ==, ===, !=
+
+//Exemplo:
+
+console.log((3 > 5)) // Falso
+console.log( 3 < 5) // Verdadeiro
+console.log( 3 <= 5) // Verdadeiro
+console.log( 3 >= 5) // Falso
+console.log( 3 == 5) // Falso
+console.log( 3 === 5) // Falso
+console.log( 3 != 5) // Verdadeiro
